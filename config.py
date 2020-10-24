05192adfcca23a7aceb69c7fe5b6aae600a0696c
oq-Yuki-op <o.yukinko0715@gmail.com>
2020-10-24 04:21:38 +0000

    設定値を追加

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 class Config:
     IMAGE_SHAPE = (28, 28, 1)
     NUM_CLASSES = 10
-    MODEL_PATH = 'saved_model/arcface'
+    MODEL_NAME = 'tmp'
+    MODEL_PATH = f'results/saved_model/{MODEL_NAME}'
     EPOCH = 50
     BATCH_SIZE = 512
     CHECKPOINT_PATH = 'results/checkpoints/cp-{epoch:04d}.ckpt'
